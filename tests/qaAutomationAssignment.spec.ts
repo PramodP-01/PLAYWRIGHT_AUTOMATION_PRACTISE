6110d2fad8f92dac45feb94458236c0fc4d1726f
Pramod P <pramodrocky005@gmail.com>
2025-09-09 18:35:56 +0530

    Update Playwright automation assignment tests

--- tests/qaAutomationAssignment.spec.ts
+++ tests/qaAutomationAssignment.spec.ts
@@ -1,3 +1,9 @@
 import { test, expect } from '@playwright/test';
 
-a
+test.describe('QA Automation Assignment', () => {
+    test('should open the website', async ({ page }) => {
+        await page.goto('https://www.google.com');
+        await expect(page).toHaveTitle('Google');
+    });
+    
+}); 
