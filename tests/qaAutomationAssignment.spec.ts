82aa3ed7c7b43cc42441776dc7ac5e48419bf8ad
Pramod P <pramodrocky005@gmail.com>
2025-09-09 18:58:05 +0530

    testcases added

--- tests/qaAutomationAssignment.spec.ts
+++ tests/qaAutomationAssignment.spec.ts
@@ -1,9 +1,13 @@
 import { test, expect } from '@playwright/test';
 
 test.describe('QA Automation Assignment', () => {
-    test('should open the website', async ({ page }) => {
-        await page.goto('https://www.google.com');
-        await expect(page).toHaveTitle('Google');
+    test('Open a webpage and take a screenshot', async ({ page }) => {
+        await page.goto('https://example.com');
+        await page.screenshot({ path: 'example.png',fullPage: true });
+        await page.waitForTimeout(10000);
+        await page.close();
+
     });
-    
+
+
 }); 
