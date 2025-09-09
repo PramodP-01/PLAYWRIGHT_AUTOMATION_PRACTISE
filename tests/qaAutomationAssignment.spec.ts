90e9b0e2956e2a5c19c3f1440038c7b950fb7ae5
Pramod P <pramodrocky005@gmail.com>
2025-09-09 18:58:41 +0530

    testcases added

--- tests/qaAutomationAssignment.spec.ts
+++ tests/qaAutomationAssignment.spec.ts
@@ -9,5 +9,15 @@ test.describe('QA Automation Assignment', () => {
 
     });
 
+    test('Login form automation', async ({ page }) => {
+        await page.goto('https://www.saucedemo.com')
+        await page.fill('input[id="user-name"]', 'standard_user')
+        await page.fill('input[id="password"]', 'secret_sauce')
+        await page.click('input[id="login-button"]')
+        await page.waitForTimeout(10000);
+        await page.close();
+
+    });
+
 
 }); 
